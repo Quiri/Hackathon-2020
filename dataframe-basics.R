8884c8c0a6da23537e9fdc2d7efb7d9057d5bc72
Kirill Pomogajko <kirill@adblockplus.org>
2020-11-14 12:10:58 +0100

    initial commit

---
+++ dataframe-basics.R
@@ -0,0 +1,17 @@
+# install.packages("tidyverse")
+# install.packages("shiny")
+
+library(tidyverse)
+welcome <- "Willkommen im Kurs"
+
+tolower(substr(gsub("[AEIOU]", "A", toupper("Willkommen im Kurs")), 1,4))
+
+gsub("[AEIOU]", "A", toupper(welcome))
+
+welcome %>% toupper() %>% gsub("[AEIOU]", "A", .) %>% substr(1, 4) %>% tolower()
+
+iris %>%
+  filter(Species =="setosa")%>%
+  select(Petal.Width, Petal.Length, Species) %>%
+  mutate(Sum = Petal.Length + Petal.Width) %>%
+  summarize(mean = mean(Sum), max = max(Sum))
